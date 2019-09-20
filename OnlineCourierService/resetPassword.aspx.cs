a6affab63e31f655877e669e753bd47ca0383020
Kuntal Das <25ckuntaldas@gmail.com>
2019-09-21 00:48:57 +0530

    format correction

--- OnlineCourierService/resetPassword.aspx.cs
+++ OnlineCourierService/resetPassword.aspx.cs
@@ -1,10 +1,6 @@
 ﻿using OnlineCourierService.Class;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.Web.UI;
-using System.Web.UI.WebControls;
 
 namespace OnlineCourierService
 {
